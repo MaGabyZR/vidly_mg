67b943d253587391f8451a208bf52aee757db470
Gabriela Rubio <gabyrubioa@gmail.com>
2025-02-21 13:12:03 -0600

    Part III. Replace the genres[ ] with MongoDB.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose'); //load mongoose to connect to MongoDB.
 const Joi = require('joi'); //load de joi module, for input validation, it returns a class.
 const genres = require('./routes/genres'); //load the genres module. 
 const express = require('express'); //load the express module
 const app = express(); //by default we store the result in a constant called app, to represent our application.
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json()); //to enable parsing of json objects in the body of a post request. 
 app.use('/api/genres', genres); ////to use the genres router, and remove all /api/genres route in genres.js
--- routes/genres.js
+++ routes/genres.js
@@ -1,61 +1,74 @@
+const mongoose = require('mongoose'); //load mongoose to define the Schema.
 const express = require('express'); //load the Express module.
 const router = express.Router(); //to call express in this separate module.Here you work with a router object, instead of an app object. 
 const Joi = require('joi'); //load de joi module, for input validation, it returns a class.
 
-//Define the genres array.
-const genres = [
-    { id: 1, name: 'Action' },  
-    { id: 2, name: 'Horror' },  
-    { id: 3, name: 'Romance' },  
-  ];
+//Create a model and Define the schema for the genres.
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
 
-//Define a route.
-router.get('/', (req, res) => {
+//Define a route and return all the genres in the db.
+router.get('/', async(req, res) => {
+    const genres = await Genre.find().sort('name');
     res.send(genres);
   });
 
 //Define a post request and its path, and create a new course object and push it on the array.
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validateGenre(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
   
-    const genre = {
-      id: genres.length + 1,
-      name: req.body.name
-    };
-    genres.push(genre);
+    //Change the object to a the new model, delete the id, as it managed by Mongo. Save the new object to the db.
+    let genre = new Genre({name: req.body.name });
+    genre = await genre.save();
+    
     res.send(genre);
   });
 
 //To update a genre, add a new route handler.1.Look up the genre and if it doesn´t exist return 404. 
 // Validate it or return 400 and if valid update it and return it.
-router.put('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
-    const { error } = validateGenre(req.body); 
+//Use the update first approach, to update documents in Mongo. 
+router.put('/:id', async (req, res) => {
+  const { error } = validateGenre(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  });
+  
+    //const genre = genres.find(c => c.id === parseInt(req.params.id)); //Replaced in the findbyidandupdate.
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
     
-    genre.name = req.body.name; 
+    //genre.name = req.body.name; ////Replaced in the findbyidandupdate.
     res.send(genre);
   });
   
 //1.look up the genre, if it doesn´t exist return 404.Than delete it and return the same course.
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+//Use the findByIdAndRemove method. 
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+    // const genre = genres.find(c => c.id === parseInt(req.params.id)); //you don´t need to look a genre in an array anymore. 
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
+    /* const index = genres.indexOf(genre); //this two lines are for working with an array, not a database. 
+    genres.splice(index, 1); */
   
     res.send(genre);
   });
   
-//GET Request. To GET a specific genre, use a route parameter and in this case we use id as the parameter. 
+//GET Request. To GET a specific genre, use a route parameter and in this case we use id as the parameter. Use the findById approach.
 //And return a 404 error if the course is not find.
-router.get('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+    //const genre = genres.find(c => c.id === parseInt(req.params.id)); //We do not need it, as this is only for working with arrays, not db.
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    
     res.send(genre);
   });
 

3af411483d82e949e2099f5169acc6ee760eb90e
Gabriela Rubio <gabyrubioa@gmail.com>
2025-02-27 11:59:15 -0600

    Part VI: building the rentals API with a POST and GET request.

---
+++ models/rental.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose'); //load mongoose to define the Schema.
+const Joi = require('joi'); //load de joi module, for input validation, it returns a class.
+
+//Create a model and Define the schema for the rentals, and put it in a separate schema. Only use the properties needed for the rentals and
+// not all properties included in the customer and movie module. This why you create a new customer and movie schema here. 
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      }
+    }),
+    required: true
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        minlength: 0,
+        maxlength: 255
+      }
+    }),
+    required: true
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
+  }
+})
+
+const Rental = mongoose.model('Rental', rentalSchema);
+
+//Function to reuse validation.This is the new updated code for Joi v16.
+// The validate() method is now used directly on the schema object, rather than being called as a static method on Joi.
+//This are the values the customer sends to the server.
+function validateRental(rental) {
+    const schema = Joi.object({
+      customerId: Joi.string().required(),
+      movieId: Joi.string().required()
+    });
+  
+    return schema.validate(rental, schema); 
+  };
+
+exports.rentalSchema = rentalSchema; 
+exports.Rental = Rental;
+exports.validate = validateRental;
---
+++ routes/rentals.js
@@ -0,0 +1,61 @@
+const {Rental, validate} = require('../models/rental');
+const {Movie} = require('../models/movie');
+const{Customer} = require('../models/customer');
+const express = require('express'); //load the Express module.
+const router = express.Router(); //to call express in this separate module.Here you work with a router object, instead of an app object. 
+
+//Define a route and return all the rentals in the db.
+router.get('/', async(req, res) => {
+    const rentals = await Rental.find().sort('-dateOut');
+    res.send(rentals);
+  });
+
+//Define a post request and its path, and create a new rental object and push it on the array.
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Verify that the customerId is valid
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid Customer!');
+
+    //Verify that he movieId is valid.
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(400).send('Invalid Movie!');
+
+    //Verify if the movie is available.
+    if (movie.numberInStock === 0) return res.status(400).send('Movie not available!');
+  
+    //Create a new rental object. 
+    let rental = new Rental({
+      customer: {
+        _id: customer._id,
+        name: customer.name,
+        phone: customer.phone
+      },
+      movie: {
+        _id: movie._id,
+        title: movie.title,
+        dailyRentalRate: movie.dailyRentalRate
+      }
+    }); 
+    rental = await rental.save();
+
+    //update the numberInStock and save it.
+    movie.numberInStock--;
+    movie.save();
+    
+    res.send(rental);
+  });
+
+//GET Request. To GET a specific rental, use a route parameter and in this case we use id as the parameter. Use the findById approach.
+//And return a 404 error if the rental is not found.
+router.get('/:id', async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+
+    if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+    
+    res.send(rental);
+  });
+
+module.exports = router; 

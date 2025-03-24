1473e6c1a8fe4efd07a0ab9d50b04e0a71af9ea3
Gabriela Rubio <gabyrubioa@gmail.com>
2025-03-24 11:39:49 -0600

    Refactoring I: extracting all the routes to routes.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
-const mongoose = require('mongoose'); //load mongoose to connect to MongoDB.
-const genres = require('./routes/genres'); //load the genres module.
-const customers = require('./routes/customers'); //load the customers module.
-const rentals = require('./routes/rentals');     //load the rentals module.
-const movies = require('./routes/movies');       //load the movies module. 
-const users = require('./routes/users');         //load the users module.
-const auth = require('./routes/auth');          //load the auth module.
-const express = require('express'); //load the express module.
-const app = express(); //by default we store the result in a constant called app, to represent our application. 
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');           //load mongoose to connect to MongoDB.
+const express = require('express');             //load the express module.
+const app = express();                          //by default we store the result in a constant called app, to represent our application. 
+
+require('./startup/routes')(app);               //load the routes module. 
 
 /* //Catch errors at a Node level.First approach:
 process.on('uncaughtException', (ex) => {
@@ -56,15 +52,6 @@ if (!config.get('jwtPrivateKey')){
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(() => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());                //to enable parsing of json objects in the body of a post request. 
-app.use('/api/genres', genres);         //to use the genres router with Express, and remove all /api/genres route in genres.js
-app.use('/api/customers', customers);   //to use the customers router with Express. 
-app.use('/api/movies', movies);         //to use the movies router with Express. 
-app.use('/api/rentals', rentals);       //to use the rentals router with Express. 
-app.use('/api/users', users);           //to use the users router with Express. 
-app.use('/api/auth', auth);             //to use the auth router with Express. 
-app.use(error);                         //Handle all the errors in the app, with this error handling middleware function.
 
 //Add an environment variable, so you can listen to the port dinamically. On the terminal set PORT=5000 
 const port = process.env.PORT || 3000;
---
+++ startup/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');                //load the express module.
+const genres = require('../routes/genres');        //load the genres module.
+const customers = require('../routes/customers'); //load the customers module.
+const rentals = require('../routes/rentals');     //load the rentals module.
+const movies = require('../routes/movies');       //load the movies module. 
+const users = require('../routes/users');         //load the users module.
+const auth = require('../routes/auth');          //load the auth module.
+const error = require('../middleware/error');
+
+
+module.exports = function(app){
+    app.use(express.json());                //to enable parsing of json objects in the body of a post request. 
+    app.use('/api/genres', genres);         //to use the genres router with Express, and remove all /api/genres route in genres.js
+    app.use('/api/customers', customers);   //to use the customers router with Express. 
+    app.use('/api/movies', movies);         //to use the movies router with Express. 
+    app.use('/api/rentals', rentals);       //to use the rentals router with Express. 
+    app.use('/api/users', users);           //to use the users router with Express. 
+    app.use('/api/auth', auth);             //to use the auth router with Express. 
+    app.use(error);                         //Handle all the errors in the app, with this error handling middleware function.
+};

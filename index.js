5fdae3769e3f65f1fc0d1ad3cb6d60c369e7ca38
Gabriela Rubio <gabyrubioa@gmail.com>
2025-03-24 12:33:31 -0600

    Refactoring V: extracting validation logic.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 const express = require('express');             //load the express module.
 const app = express();                          //by default we store the result in a constant called app, to represent our application. 
 
 require('./startup/logging');                   //load the logging module first, in case you get an error.
 require('./startup/routes')(app);               //load the routes module and app. 
 require('./startup/db')();                      //load the DB.
-require('./startup/config')();                    //load the config module.
+require('./startup/config')();                  //load the config module.
+require('./startup/validation')();              //load the validation module.
 
 //Add an environment variable, so you can listen to the port dinamically. On the terminal set PORT=5000 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log (`Listening on port ${port}...`));
+app.listen(port, () => {
+    winston.info(`Listening on port ${port}...`);
+    console.log(`Listening on port ${port}...`); // Fallback in case Winston fails
+});

9a9844a6901553fb32209f6d69882f4b7546341a
Gabriela Rubio <gabyrubioa@gmail.com>
2025-04-09 18:23:05 -0600

    II Genres Integration test, GET /:id, to test routes with parameters.

--- startup/db.js
+++ startup/db.js
@@ -6,6 +6,6 @@ const config = require('config');
 module.exports = function(){
     const db = config.get('db');
     mongoose.connect(db)
-    //.then(() => winston.info(`Connected to ${db}...`))
-    .then (console.log(`Connected to ${db}...`));
+    .then(() => winston.info(`Connected to ${db}...`));
+    //.then (console.log(`Connected to ${db}...`));
 }
--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -6,7 +6,7 @@ let server;
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); })
     afterEach(async () => { 
-        server.close();
+        await server.close();
         await Genre.deleteMany({});
     });
 
@@ -23,6 +23,18 @@ describe('/api/genres', () => {
             expect(res.body.length).toBe(2);
             expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
             expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+        });
+    });
+
+    describe('GET /:id', () => {            //Test routes with parameters.
+        it('should return a genre if valid id is passed', async () => {
+            const genre = new Genre({ name: 'genre1'});
+            await genre.save();
+
+            const res = await request(server).get('/api/genres/' + genre._id);
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
         });
     });
 });

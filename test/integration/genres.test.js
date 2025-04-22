c4bcf8662c625ad1b83b15351fc6dc00f16b7b23
Gabriela Rubio <gabyrubioa@gmail.com>
2025-04-22 11:29:20 -0600

    V Refactoring part IV, to make the code clean.

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -47,57 +47,56 @@ describe('/api/genres', () => {
     });
 
     describe( ' POST /', () => {            //Test the route handler for creating a new genre. 
-        it('Should return a 401 if client is not logged in', async () => {
-            const res = await request(server)
+
+        let token;
+        let name; 
+        
+        const execute = async () => {
+            return await request(server)
             .post('/api/genres')
-            .send({ name: 'genre1'});
+            .set('x-auth-token', token)
+            .send({ name });
+        }
+
+        beforeEach(() => {                      
+            token = new User().generateAuthToken();     //Befor each test, we initialize the token to a valid JWT.
+            name = 'genre1';
+        })
+
+        it('Should return a 401 if client is not logged in', async () => {
+            token = '';             //it is set to an empty string, because in this test we do not have a token. 
+
+            const res = await execute(); 
             
             expect(res.status).toBe(401);
         });
 
         it('Should return a 400 if genre is less than 5 characters', async () => {          //<5 characters is invalid, as per the schema in genre.js
-            const token = new User().generateAuthToken();
-            
-            const res = await request(server)
-            .post('/api/genres')
-            .set('x-auth-token', token)
-            .send({ name: '1234'});
+            name = '1234'; 
+
+            const res = await execute();
             
             expect(res.status).toBe(400);
         });
 
         it('Should return a 400 if genre is more than 50 characters', async () => {          //>50 characters is invalid, as per the schema in genre.js
-            const token = new User().generateAuthToken();
-
-            const name = new Array(52).join('a');                                           //generate a new array of 51 elements in JS to test. 
             
-            const res = await request(server)
-            .post('/api/genres')
-            .set('x-auth-token', token)
-            .send({ name: name });
+            name = new Array(52).join('a');                                                 //generate a new array of 51 elements in JS to test. 
+            
+            const res = await execute();
             
             expect(res.status).toBe(400);
         });
 
         it('Should save the genre if it is valid', async () => {                        //make sure the valid genre is saved in the db.
-            const token = new User().generateAuthToken();
-
-            const res = await request(server)
-            .post('/api/genres')
-            .set('x-auth-token', token)
-            .send({ name: 'genre1' });
+            await execute();
             
             const genre = await Genre.find({ name: 'genre1'});                          //query the db.
             expect(genre).not.toBeNull();
         });
 
         it('Should return the genre if it is valid', async () => {                        //make sure the genre is the body of the response. 
-            const token = new User().generateAuthToken();
-
-            const res = await request(server)
-            .post('/api/genres')
-            .set('x-auth-token', token)
-            .send({ name: 'genre1' });
+            const res = await execute();
             
             expect(res.body).toHaveProperty('_id');
             expect(res.body).toHaveProperty('name', 'genre1');

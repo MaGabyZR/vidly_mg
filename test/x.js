1bd4cb284689d9c4ad166d510901559c772659fc
Gabriela Rubio <gabyrubioa@gmail.com>
2025-04-24 11:42:37 -0600

    TDD I: Defining the test cases in x.js and implementing the set up for returns.test.js

---
+++ test/x.js
@@ -0,0 +1,16 @@
+//Test cases for returning a rental.
+
+// POST /api/returns {customerId, movieId}
+
+//Error scenarios:
+    //Return 401 if client is not logged in.
+    //Return 400 if customerId is not provided.
+    //Return 400 if movieId is not provided.
+    //Return 404 if no rental is found for this customer or movie.
+    //Return 400 if the rental has already been processed, the customer already returned the movie.
+//Sucess scenarios:
+    //Return 200 if it is a velid request.
+    //Set the return date.
+    //Calculate the rental fee.
+    //Increase the stock, when a movie is back.
+    //Finally, return the summary of the rental. 
